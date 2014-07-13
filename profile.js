8e266aefe10ff5aee22bf739524914ede256cda3
Jamie Patel <jamiepatel@Jamies-MacBook-Pro.local>
2014-07-13 14:20:50 +0100

    Work and picks preview done

--- profile.js
+++ profile.js
@@ -8,6 +8,7 @@ app.controller('PreviewController', function($scope){
 			description: "This is a short description of the art-work for the profile preview.",
 			recommendations: 49,
 			additionalInformation: "Commissioned",
+			commissionedOrFeatured: true,
 			image: "./public/chillies.jpg"
 		},
 		{
@@ -15,6 +16,7 @@ app.controller('PreviewController', function($scope){
 			description: "This is a short description of the art-work for the profile preview.",
 			recommendations: 193,
 			additionalInformation: "Commissioned",
+			commissionedOrFeatured: true,
 			image: "./public/room.jpg",
 		},
 		{
@@ -22,6 +24,7 @@ app.controller('PreviewController', function($scope){
 			description: "This is a short description of the art-work for the profile preview.",
 			recommendations: 121,
 			additionalInformation: "Featured in Notes",
+			commissionedOrFeatured: true,
 			image: "./public/street.jpg"
 		}
 
@@ -32,19 +35,25 @@ app.controller('PreviewController', function($scope){
 		title: "Poetry in Cambridge",
 		description: "This is a short description of the anthology for the profile preview",
 		additionalInformation: "Anthology",
-		image: ""
+		anthology: true,
+		recommendations: 29,
+		image: "http://upload.wikimedia.org/wikipedia/commons/f/fd/KingsCollegeChapel.jpg"
 	},
 	{
 		title: "This Should Be So",
 		description: "This is a short description of the art-work for the profile preview",
 		additionalInformation: "By Matthew Neal",
-		image: ""
+		singleAuthor: true,
+		recommendations: 362,
+		image: "./public/matt.jpg"
 	},
 	{
 		title: "Matisse and his Birds",
 		description: "This is a short description of the art-work for the profile preview",
 		additionalInformation: "By Jemima Moore",
-		image: ""
+		singleAuthor: true,
+		recommendations: 91,
+		image: "./public/matisse.jpg"
 	}
 
 
