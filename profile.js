a8b1ce27f8f410fbe845b53cfced509c1e86b5ea
Jamie Patel <jamiepatel@Jamies-MacBook-Pro.local>
2014-07-13 01:25:38 +0100

    Started using angular

---
+++ profile.js
@@ -0,0 +1,31 @@
+app.controller('ProfileController', function(){
+
+	var workSelection = [
+		{
+			title: "Chillies",
+			description: "This is a short description of the art-work for the profile preview.",
+			recommendations: 49,
+			additionalInformation: "Commissioned",
+			image: "./public/chillies.jpg"
+		},
+		{
+			title: "Magic",
+			description: "This is a short description of the art-work for the profile preview.",
+			recommendations: 193,
+			additionalInformation: "Commissioned",
+			image: "./public/room.jpg",
+		},
+		{
+			title: "New York",
+			description: "This is a short description of the art-work for the profile preview.",
+			recommendations: 121,
+			additionalInformation: "Featured in <a href='#'>Notes</a>",
+			image: "./public/street.jpg"
+		}
+
+	];
+
+
+
+
+});
