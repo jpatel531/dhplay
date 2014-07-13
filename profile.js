d9f6be13d530cd3e387197bd089f99d998481526
Jamie Patel <jamiepatel@Jamies-MacBook-Pro.local>
2014-07-13 12:32:37 +0100

    Ng-mouseenter working

--- profile.js
+++ profile.js
@@ -2,7 +2,7 @@ var app = angular.module('profile', []);
 
 app.controller('PreviewController', function($scope){
 
-	var workSelection = [
+	$scope.workSelection = [
 		{
 			title: "Chillies",
 			description: "This is a short description of the art-work for the profile preview.",
@@ -27,7 +27,31 @@ app.controller('PreviewController', function($scope){
 
 	];
 
-	this.selection = workSelection;
+	$scope.pickSelection = [
+	{
+		title: "Poetry in Cambridge",
+		description: "This is a short description of the anthology for the profile preview",
+		additionalInformation: "Anthology",
+		image: ""
+	},
+	{
+		title: "This Should Be So",
+		description: "This is a short description of the art-work for the profile preview",
+		additionalInformation: "By Matthew Neal",
+		image: ""
+	},
+	{
+		title: "Matisse and his Birds",
+		description: "This is a short description of the art-work for the profile preview",
+		additionalInformation: "By Jemima Moore",
+		image: ""
+	}
+
+
+	];
+
+	this.selection = $scope.workSelection;
+
 
 
 });
